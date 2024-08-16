de6349a8f44b6144a1f61f370dce615bc38f6783
Enrique-mac <carvalhoenrique33@gmail.com>
2024-08-16 10:44:12 -0300

    aula 01

--- aula1/atividade1/variaveisOperadores.js
+++ aula1/atividade1/variaveisOperadores.js
@@ -2,22 +2,22 @@
 let n1 = 3;
 let n2 = 5;
 let result = n1 + n2;
-console.log(result);
+console.log(`Resultado da adicao entre ${n1} e ${n2} = ${result}`);
 
 // subtracao
 let n1Sub = 6;
 let n2Sub = 5;
 let resultSub = n1Sub - n2Sub;
-console.log(resultSub);
+console.log(`Resultado da subtracao entre ${n1Sub} e ${n2Sub} = ${resultSub}`);
 
 // multiplicacao
 let n1Mult = 6;
 let n2Mult = 5;
 let resultMult = n1Mult * n2Mult;
-console.log(resultMult);
+console.log(`Resultado da multiplicacao entre ${n1Mult} e ${n2Mult} = ${resultMult}`);
 
 // divisao
 let n1Div = 6;
 let n2Div = 3;
 let resultDiv = n1Div / n2Div;
-console.log(resultDiv);
+console.log(`Resultado da divisao entre ${n1Div} e ${n2Div} = ${resultDiv}`);
