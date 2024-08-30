8767981ca5d8c56a17839e4bb25106198c60ddba
Enrique-mac <carvalhoenrique33@gmail.com>
2024-08-30 10:32:14 -0300

    correção do exercicio

--- aula2/atividade2/matrizTransposta.js
+++ aula2/atividade2/matrizTransposta.js
@@ -2,15 +2,25 @@
 // A função deve receber uma matriz (array de arrays) A e imprimir a matriz original e a versão transposta
 
 function transporMatriz(A) {
-    console.log("Matriz original: " + A);
+    let A_string = "";
+    let At_string = "";
 
     for(let i = 0; i < A.length; i++) {
         for(let j = 0; j < A[i].length; j++) {
-            // por algum motivo a linah de baixo nao funcionou ;(
-            // process.stdout.write(A[i][j]);
-            console.log(A[i][j]);
+            A_string += `${A[i][j]} `;
         }
+        A_string += "\n";
     }
+
+    for(let i = 0; i < A[0].length; i++) {
+        for(let j = 0; j < A.length; j++) {
+            At_string += `${A[j][i]} `;
+        }
+        At_string += "\n";
+    }
+
+    console.log("Matriz original: \n" + A_string);
+    console.log("Matriz transposta: \n" + At_string);
 }
 
 let matriz = [[1, 2], [3, 4], [5, 6]];
